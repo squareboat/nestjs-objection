99db1c9847ab4b478eb521ffcc32be515a0cd3ec
Vinayak Sarawagi <vinayaksarawagi25@gmail.com>
2023-07-16 03:48:32 +0530

    ADD: validate connection at boot

--- lib/service.ts
+++ lib/service.ts
@@ -1,17 +1,17 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { BaseModel } from './baseModel';
-import { SquareboatNestObjection } from './constants';
-import { DatabaseOptions } from './options';
-import Knex, { Knex as KnexType } from 'knex';
-import { ConnectionNotFound } from './exceptions';
+import { Inject, Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { BaseModel } from "./baseModel";
+import { SquareboatNestObjection } from "./constants";
+import { DatabaseOptions } from "./options";
+import Knex, { Knex as KnexType, knex } from "knex";
+import { ConnectionNotFound } from "./exceptions";
 
 @Injectable()
-export class ObjectionService {
+export class ObjectionService implements OnModuleInit {
   static config: DatabaseOptions;
   static dbConnections: Record<string, KnexType>;
 
   constructor(
-    @Inject(SquareboatNestObjection.databaseOptions) config: DatabaseOptions,
+    @Inject(SquareboatNestObjection.databaseOptions) config: DatabaseOptions
   ) {
     const defaultConnection = config.connections[config.default];
     ObjectionService.config = config;
@@ -19,8 +19,28 @@ export class ObjectionService {
     BaseModel.knex(Knex(defaultConnection));
     for (const conName in config.connections) {
       ObjectionService.dbConnections[conName] = Knex(
-        config.connections[conName],
+        config.connections[conName]
       );
+    }
+  }
+
+  async onModuleInit() {
+    for (const connName in ObjectionService.dbConnections) {
+      console.debug(
+        `[@squareboat/nestjs-objection] '${connName}' validating connection...`
+      );
+      const connection = ObjectionService.dbConnections[connName];
+      try {
+        const result = await connection.raw("select 1+1 as result");
+        console.debug(
+          `[@squareboat/nestjs-objection] '${connName}' connection validated...`
+        );
+      } catch (_e) {
+        const e = _e as Error;
+        console.error(
+          `[@squareboat/nestjs-objection] '${connName}' connection failed, REASON: ${e.message}`
+        );
+      }
     }
   }
 
@@ -29,7 +49,7 @@ export class ObjectionService {
     conName = conName || ObjectionService.config.default;
 
     const isConNameValid = Object.keys(
-      ObjectionService.config.connections,
+      ObjectionService.config.connections
     ).includes(conName);
 
     if (conName && !isConNameValid) {
